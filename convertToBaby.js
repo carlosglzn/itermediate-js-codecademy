afbdfb6d6e8fff82209d4fb3061e011721fb4210
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-09-30 15:58:33 -0500

    convertToBaby function

---
+++ convertToBaby.js
@@ -0,0 +1,15 @@
+// Write a function, convertToBaby(), that takes in an array as an argument and, using a loop, returns a new array with each string in the array prepended with 'baby '.
+
+const convertToBaby = (arr) => {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(`baby ${arr[i]}`);
+  }
+  return newArr;
+};
+
+// When you're ready to test your code, uncomment the below and run:
+
+const animals = ["panda", "turtle", "giraffe", "hippo", "sloth", "human"];
+
+console.log(convertToBaby(animals));
